3bf1474d5be9c60e4198c6c6ab7de2da32bdd697
tristanahayhay-ship-it <tristanahayhay@gmail.com>
2026-07-19 04:06:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1309,7 +1309,7 @@ elif menu == "Công Cụ Hỗ Trợ & Demo Trade":
     <body>
         <div class="tradingview-widget-container" style="width: 100%; height: 100%;">
             <div class="tradingview-widget-container__widget"></div>
-            <script type="text/javascript" src="https://tradingview.com" async>
+            <script type="text/javascript" src="https://s3.tradingview.com/tv.js" async>
             {
                 "interval": "1m",
                 "width": "100%",
